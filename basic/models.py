cdd74a0fa683a008a27fa420c2c9fc022467c01e
Ganesh Baliga <baliga@rowan.edu>
2024-02-13 11:00:37 -0500

    Created model Compute in the basic app. Created a url route for computing the square of a number.

--- basic/models.py
+++ basic/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+# Computed objects cache computations that were already performed
+# A computation that has already been performed will not be performed again 
+class Computed(models.Model):  
+    input = models.IntegerField()
+    output = models.IntegerField()
+
+
--- basic/views.py
+++ basic/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.http import Http404
+from .models import Computed
 
 # Create your views here.
+
+# Computation function
+def compute(request, value):
+    try:
+        input = int(value)
+        precomputed = Computed.objects.filter(input=input)
+        if precomputed.count() != 0:  
+            # This was already computed, so look up answer
+            answer = precomputed[0].output
+        else:
+            # Compute the answer
+            answer = input * input
+            # Save it into the database
+            computed = Computed(input=input, output=answer)
+            computed.save()
+    except:
+        raise Http404(f"Invalid input: {value}")
+
+    return render (
+        request,
+        "basic/compute.html",
+        {
+            'input': input,
+            'output': answer
+        }
+    )
