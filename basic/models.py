b6385defc98fb2152214424f334dd38d963d0431
Ganesh Baliga <baliga@rowan.edu>
2024-02-13 12:21:59 -0500

    Updated model Compute to include the time that a computation was performed

--- basic/models.py
+++ basic/models.py
@@ -7,5 +7,9 @@ from django.db import models
 class Computed(models.Model):  
     input = models.IntegerField()
     output = models.IntegerField()
+    time_computed = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return(f"{self.input} -> {self.output}")
 
 
--- basic/views.py
+++ basic/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import Http404
 from .models import Computed
+from django.utils import timezone
 
 # Create your views here.
 
@@ -12,12 +13,18 @@ def compute(request, value):
         if precomputed.count() != 0:  
             # This was already computed, so look up answer
             answer = precomputed[0].output
+            time_computed = precomputed[0].time_computed
         else:
             # Compute the answer
             answer = input * input
+            time_computed = timezone.now()
             # Save it into the database
-            computed = Computed(input=input, output=answer)
-            computed.save()
+            computed = Computed(
+                input=input, 
+                output=answer,
+                time_computed=time_computed
+            )
+            computed.save() # Store it into the database
     except:
         raise Http404(f"Invalid input: {value}")
 
@@ -26,6 +33,8 @@ def compute(request, value):
         "basic/compute.html",
         {
             'input': input,
-            'output': answer
+            'output': answer,
+            'time_computed': time_computed
+
         }
     )
