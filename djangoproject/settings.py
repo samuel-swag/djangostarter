9fcaaceafc50204f02a1af755882cced35add23c
Ganesh Baliga <baliga@rowan.edu>
2024-03-03 08:22:33 -0500

    Added tasks app

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "tasks.apps.TasksConfig",
     "htmx.apps.HtmxConfig",
     "basic.apps.BasicConfig",
     "django.contrib.admin",
